1ca6aa467821a486f755aa97d522c302e4cfd83c
masum_devs <mehedieasital@gmail.com>
2024-01-28 16:18:57 +0600

    initial commit

---
+++ app/src/main/java/com/mehedi/beeda_workmanagerapp/NotificationUtil.kt
@@ -0,0 +1,18 @@
+package com.mehedi.beeda_workmanagerapp
+
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import com.mehedi.beeda_workmanagerapp.MainActivity.Companion.CHANNEL_ID
+
+fun NotificationManager.sendNotification(context: Context, title: String = "Succeed", desc : String = "Worker is completed") {
+
+    val NOTIFY_ID = 42633687
+
+    val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+        .setContentTitle(title)
+        .setContentText(desc)
+        .setSmallIcon(R.drawable.ic_baseline_celebration_24)
+
+    notify(NOTIFY_ID, notification.build())
+}
